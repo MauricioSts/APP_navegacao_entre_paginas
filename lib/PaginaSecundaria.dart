d29985f6a016038bed2e4d31de14fc86037c4544
MauricioSts <mauricio.santos.110@ufrn.edu.br>
2025-03-16 15:34:40 -0300

    aprendendo navegação

---
+++ lib/PaginaSecundaria.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Paginasecundaria extends StatefulWidget {
+  const Paginasecundaria({super.key});
+
+  @override
+  State<Paginasecundaria> createState() => _PaginasecundariaState();
+}
+
+class _PaginasecundariaState extends State<Paginasecundaria> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text("Tela principal"),
+      ),
+      body: Container(
+        child: Column(children: [Text("pagina secundariaaaaaa")]),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao/PaginaSecundaria.dart';
+
+void main() {
+  runApp(MaterialApp(home: TelaPrincipal()));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({super.key});
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: Text("Tela principal"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Paginasecundaria()),
+                );
+              },
+              child: Text("Ir para proxima pagina"),
+              style: TextButton.styleFrom(backgroundColor: Colors.tealAccent),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
